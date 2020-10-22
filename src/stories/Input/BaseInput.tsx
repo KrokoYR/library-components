eb759baa46823bf669a1f5cd15fed579aecbf632
Lopson <balzhinimaev-2011@mail.ru>
2020-10-22 12:13:06 +0300

    Initial commit

---
+++ src/stories/Input/BaseInput.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// TODO: Research https://github.com/sanniassin/react-input-mask
+const BaseInput = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BaseInput;
